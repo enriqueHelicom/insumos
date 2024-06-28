91ec41793d87afbbb5e90b5aa9499a3c2fc2781e
enrique cruz <lcruz@syshelicom.mx>
2024-06-28 16:49:27 -0600

    product fix

--- static/js/cart.js
+++ static/js/cart.js
@@ -4,20 +4,58 @@ export function cartShopping(){
     btnAddCart.on("click", function(){
     
         let productId = $(this).attr("data-idProduct");
-        console.log(productId);
+        let countProductValue = $(".count-product .value");
 
+        let formData = new FormData();
+        formData.append('id',productId);
 
-        $.ajax({
-            type: "get",
-            url: "http://127.0.0.1:5500/db.json",
-            dataType: "json",
-            success: function (response) {
-                  
-            }
-        });
-
-        btnAddCart.attr("disabled",true);
+        fetchData(formData, countProductValue.text());
+        
         
 
     });
+
+
+}
+
+// Peticion asyn
+function fetchData(data, count){
+    fetch('http://192.168.10.48/insumos_termicos/static/db/connection.php',{
+        method: "POST",
+        body: data
+    })
+        .then(function(response){
+            // Si ocurre un error en la solicitud
+            if(response.status !== 200){
+                console.log("Error al realizar la solicitud");
+                return
+            }
+
+            // si la peticion es exitosa
+            response.json().then(function(data){
+                renderCard(data, count);
+            });
+        })
+        .catch(function(err){
+            console.log('fetch :-S', err);
+        })
+}
+
+
+const renderCard = (data, count) =>{
+    const product = {
+        "id":data.id,
+        "name":data.name,
+        "cantidad": count
+    };
+
+
+    sendWhatsapp(product);
+}
+
+const sendWhatsapp = (product) =>{
+    var url = "whatsapp://send?text="+encodeURIComponent("product")+"&phone="+encodeURIComponent("+525548722671")
+
+    window.open(url);
+
 }
